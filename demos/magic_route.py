65d17cebccfcf653df334ddd8a4c0e40616de504
Hanaasagi <ambiguous404@gmail.com>
2017-06-19 20:34:15 +0800

    add magic_route demo

---
+++ demos/magic_route.py
@@ -0,0 +1,17 @@
+from imouto.web import Application
+from imouto.magicroute import GET, POST
+
+
+async def hello_world_get(request, response):
+    response.write("Hello World, it'is get")
+
+
+async def hello_world_post(request, resposne):
+    resposne.write("Hello World, it'is post")
+
+
+GET / '/' > hello_world_get
+POST / '/' > hello_world_post
+
+app = Application()
+app.run()

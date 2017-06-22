601fd19f88241baf8f32c09b43544acf9bada325
Hanaasagi <ambiguous404@gmail.com>
2017-06-22 17:25:44 +0800

    add metadata

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 setup(
     name='imouto',
+    version='0.1.1',
+    description='async web frameword',
+    url='https://github.com/Hanaasagi/imouto',
+    author='Hanaasagi',
+    author_email='ambiguous404@gmail.com',
     packages=['imouto'],
     license='BSD3',
+    platforms=['Linux'],
 )

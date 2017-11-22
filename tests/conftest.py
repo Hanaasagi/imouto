c98016bd8c41e92694830b3d75a58ac4a6f4848c
Hanaasagi <ambiguous404@gmail.com>
2017-11-22 23:00:25 +0900

    pytest

---
+++ tests/conftest.py
@@ -0,0 +1,84 @@
+import gc
+import asyncio
+import pytest
+from asyncio import test_utils
+from imouto.util import tob, hkey
+
+
+class Client(test_utils.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.loop = asyncio.new_event_loop()
+        self.set_event_loop(self.loop)
+
+    def tearDown(self):
+        # just in case if we have transport close callbacks
+        test_utils.run_briefly(self.loop)
+
+        self.loop.close()
+        # clear
+        type(self.app)._instances = {}
+        gc.collect()
+        super().tearDown()
+
+    def _generate_request(self, method=b'GET', path=b'/', version=b'1.1',
+                          accept=b'*/*', accept_encoding=b'gzip, deflate',
+                          connection=b'Keep-alive', data=b'', **options):
+        headers = [b'%b: %b\r\n' % (tob(hkey(key)), tob(value))
+                   for key, value in options.items()]
+        return (b'%b %b HTTP/%b\r\n'
+                b'Accept: %b\r\n'
+                b'Accept-Encoding: %b\r\n'
+                b'Connection: %b\r\n'
+                b'%b'
+                b'\r\n'
+                b'%b' % (
+                    method, path, version,
+                    accept,
+                    accept_encoding,
+                    connection,
+                    b''.join(headers),
+                    data))
+
+    def feed(self, app):
+        self.app = app
+
+    def _get_response(self, request_data):
+
+        async def client(addr, loop, request_data):
+            reader, writer = await asyncio.open_connection(*addr, loop=loop)
+            # send a line
+            writer.write(request_data)
+            # read it back
+            response_data = await reader.read()
+            writer.close()
+            return response_data
+
+        server, addr = self.app.test_server(self.loop)
+        response_data = self.loop.run_until_complete(
+            asyncio.Task(client(addr, self.loop, request_data),
+                         loop=self.loop))
+        server.close()
+        self.loop.run_until_complete(server.wait_closed())
+        return response_data
+
+    def get(self, path, **headers):
+        request = self._generate_request(path=tob(path),
+                                         method=b'GET', **headers)
+        response = self._get_response(request)
+        return response
+
+    def post(self, path, **headers):
+        request = self._generate_request(path=tob(path),
+                                         method=b'POST', **headers)
+        response = self._get_response(request)
+        return response
+
+
+@pytest.fixture
+def client():
+    client = Client()
+    client.setUp()
+    yield client
+    client.tearDown()
---
+++ tests/test_imouto.py
@@ -0,0 +1,118 @@
+from imouto.web import RequestHandler, Application
+from imouto.magicroute import GET
+
+
+def test_basic(client):
+    class HelloWorldHandler(RequestHandler):
+
+        async def get(self):
+            self.write("Hello World")
+
+    app = Application([
+        (r'/', HelloWorldHandler),
+    ])
+    client.feed(app)
+    response = client.get('/')
+    response_correct = [
+        b'HTTP/1.1 200 OK',
+        b'Content-Type: text/html',
+        b'Content-Length: 11',
+        b'',
+        b'Hello World'
+    ]
+    assert sorted(response.split(b'\r\n')) == \
+        sorted(response_correct)
+
+
+def test_redirect(client):
+    class RedirectHandler(RequestHandler):
+
+        async def get(self):
+            self.redirect('/')
+
+    app = Application([
+        (r'/redirect/', RedirectHandler),
+    ])
+    client.feed(app)
+    response = client.get('/redirect/')
+    assert b'302 Found' in response
+    assert b'Location: /' in response
+
+
+def test_magic_route(client):
+    async def Ahandler(res, req):
+        req.write('It is magic route')
+    GET / '/magic/' > Ahandler
+
+    client.feed(Application())
+    response = client.get('/magic/')
+    assert b'It is magic route' in response
+
+
+def test_http_method(client):
+    class MethodHandler(RequestHandler):
+
+        async def get(self):
+            self.write('GET')
+
+        async def post(self):
+            self.write('POST')
+    app = Application([
+        (r'/method/', MethodHandler),
+    ])
+    client.feed(app)
+    response = client.get('/method/')
+    assert b'GET' in response
+    response = client.post('/method/')
+    assert b'POST' in response
+
+
+def test_dynamic_route(client):
+    class DynamicHandler(RequestHandler):
+
+        async def get(self, id_):
+            self.write('id: ' + id_)
+
+    app = Application([
+        (r'/(\d+)/', DynamicHandler),
+    ])
+    client.feed(app)
+    response = client.get('/2333/')
+    assert b'id: 2333', response
+
+
+def test_get_query_string(client):
+    class QueryHandler(RequestHandler):
+
+        async def get(self):
+            a = self.get_query_argument('a')
+            b = self.get_query_argument('b')
+            sum_ = int(a) + int(b)
+            self.write('sum: {}'.format(sum_))
+
+    app = Application([
+        (r'/query/', QueryHandler),
+    ])
+    client.feed(app)
+    response= client.get('/query/?a=22&b=33')
+    assert b'sum: 55', response
+
+
+def test_post_data(client):
+    class PostHandler(RequestHandler):
+        async def post(self):
+            a = self.get_body_argument('a')
+            b = self.get_body_argument('b')
+            product = int(a) * int(b)
+            self.write('product: {}'.format(product))
+
+    app = Application([
+        (r'/post/', PostHandler),
+    ])
+    client.feed(app)
+    content_type = b'application/x-www-form-urlencoded; charset=utf-8'
+    content_length = b'9'
+    response = client.post('/post/', data=b'a=33&b=22',
+                           content_type=content_type,
+                           content_length=content_length)
+    assert b'product: 726', response
